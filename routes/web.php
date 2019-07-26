08481a2f4f716bf145f39ca1fed391ed1c822b17
sarah <fhb170995@fh-vie.ac.at>
2019-07-26 16:33:58 +0200

    us43 statt us40 adminlogin erstellt
    route für admin erstellt + template von userlogin übernommen

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/admin', 'HomeController@index')->name('home');
+
+Route::get('/admin', ['as'=>'adminlogin', function() {
+    return view ('auth.login');
+}]);
